863450c19ab139b09127a89d1190bbe92492d7b5
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-05-22 22:19:55 +0300

    add different file formats for exercise 9, in case necessary

---
+++ ex1.9.js
@@ -0,0 +1,48 @@
+Script;
+started;
+on;
+2022 - 05 - 21;
+15;
+53;
+35 + 0300(base);
+0;
+smadetoj;
+-fuxi274;
+~/Kurssit/DevopsWithDocker;
+[01, 32, msmadetoj];
+-fuxi274;
+[00, m, [01, 34, m, ~/Kurssit/DevopsWithDocker, [00, m$, sudo, docker, run - v, "$(pwd,
+            d, /text.log:/usr / src / app / text.log, " devopsdockeruh/simple-web-service[sudo], password]]];
+for (smadetoj; ; )
+    : Starting;
+log;
+output
+    < nil >
+    open / usr / src / app / text.log;
+is;
+a;
+directory
+    < nil >
+    open / usr / src / app / text.log;
+is;
+a;
+directory
+    < nil >
+    open / usr / src / app / text.log;
+is;
+a;
+directory
+    < nil >
+    open / usr / src / app / text.log;
+is;
+a;
+directory
+    ^ C(base);
+0;
+smadetoj;
+-fuxi274;
+~/Kurssit/DevopsWithDocker;
+[01, 32, msmadetoj];
+-fuxi274;
+[00, m, [01, 34, m, ~/Kurssit/DevopsWithDocker, [00, m$, exit,
+            Script, done, on, 2022 - 05 - 21, 15, 53, 51 + 0300]]];
